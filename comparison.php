3a20ecb0df3299652e26f468f57a3034e26f44ac
Kelsey Damier <kelsey.damier03@myhunter.cuny.edu>
2021-04-24 20:02:37 -0400

    db connect

--- comparison.php
+++ comparison.php
@@ -10,13 +10,17 @@ $result = pg_query($conn, "SELECT * FROM taxinfo WHERE states='$_POST[state]'");
 
 
 $row = pg_fetch_array($result);
-echo $row["budget"];
-echo $row["pension"];
-echo $row["healthcare"];
-echo $row["defense"];
-echo $row["education"];
-echo $row["transport"];
-echo $row["welfare"];
+echo "
+<ul>
+	<li class=\"stat-category\">Budget(m): $${row["budget"]}M </li>
+	<li class=\"stat-category\">Pension(m): $${row["pension"]}M </li>
+	<li class=\"stat-category\">Healthcare(m): $${row["healthcare"]}M </li>
+	<li class=\"stat-category\">Defense(m): $${row["defense"]}M </li>
+	<li class=\"stat-category\">Education(m): $${row["education"]}M </li>
+	<li class=\"stat-category\">Transport(m): $${row["transport"]}M </li>
+	<li class=\"stat-category\">Welfare(m): $${row["welfare"]}M </li>
+</ul>";
+
 
 pg_close($conn);
 
